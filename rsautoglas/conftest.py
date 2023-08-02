b91a418705c7d5a6d4f44cadbbfae99d4d04015e
sr08678 <sebastian.rauch@stud.th-deg.de>
2023-08-02 16:05:54 +0200

    first commit

---
+++ rsautoglas/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from rsautoglas.users.models import User
+from rsautoglas.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
